faba6ae8bc6139d28b6a523dc64a4ab0b288e4ad
Tobias Scholze <5583469+tscholze@users.noreply.github.com>
2025-07-13 11:31:19 +0200

    Remove MacCatalyst and Tizen platform support from the project

--- TriloBot.Maui/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace TriloBot.Maui;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
