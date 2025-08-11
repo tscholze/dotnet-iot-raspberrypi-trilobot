dd816ff07cb186b11461771ad7d516a45ca9d503
Tobias Scholze <5583469+tscholze@users.noreply.github.com>
2025-08-11 15:09:03 +0200

    Add JoystickPage and MainPage with SignalR integration for real-time control and monitoring

---
+++ TriloBot.Maui/Pages/JoystickPage.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriloBot.Maui.Pages;
+
+public partial class JoystickPage : ContentPage
+{
+    public JoystickPage()
+    {
+        InitializeComponent();
+    }
+}
