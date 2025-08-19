ef89e86bd59b49064f0f686bf8e5e67755e1620f
Tobias Scholze <5583469+tscholze@users.noreply.github.com>
2025-08-19 20:32:40 +0200

    Refactor FillUnderlighting method to remove RGB validation and streamline execution

--- TriloBot.Blazor/SignalR/TriloBotHub.cs
+++ TriloBot.Blazor/SignalR/TriloBotHub.cs
@@ -80,12 +80,7 @@ public class TriloBotHub : Hub
     /// <param name="b">Blue value (0-255).</param>
     public Task FillUnderlighting(int r, int g, int b)
     {
-        // Validate r, g and b that they are in range of 0-255
-        if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255)
-            throw new ArgumentOutOfRangeException("RGB values must be between 0 and 255.");
-
-        // Call function of TriloBot
-        Task.Run(() => _robot.FillUnderlighting((byte)r, (byte)g, (byte)b));
+        return Task.Run(() => _robot.FillUnderlighting((byte)r, (byte)g, (byte)b));
     }
 
     /// <summary>
